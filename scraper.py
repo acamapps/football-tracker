b63f988808b5a27955993fef3ba310c07682b0a8
acamapps <acamapps@outlook.com>
2026-09-17 20:42:33 +0100

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -60,7 +60,7 @@ def run_scraper():
     formatted_date = uk_time.strftime("%A, %d %B %Y")
     formatted_timestamp = uk_time.strftime("%d %B %Y at %H:%M UK Time")
     
-    output = f"# Who's playing today? ⚽\n"
+    output = f"# Who's playing? ⚽\n"
     output += f"## 📅 Schedule for: {formatted_date}\n"
     output += f"_Last checked: {formatted_timestamp}_\n\n"
     
